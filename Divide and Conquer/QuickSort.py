8125f51ef9a410da634c734f4a0c130a164cfe47
SadafAsad <sadaf98x@gmail.com>
2019-12-01 18:03:45 +0330

    #2 def partition -- #1 def quick_sort

--- Divide and Conquer/QuickSort.py
+++ Divide and Conquer/QuickSort.py
@@ -1,9 +1,9 @@
 def partition(ilist, low, high):
 
-    pivot_index = len(ilist) // 2
+    pivot_index = (low+high) // 2
     pivot = ilist[pivot_index]
 
-    i = 0
+    i = low
     while i <= high:
         if ilist[i] < pivot and i != pivot_index:
             temp = ilist[low]
@@ -12,3 +12,17 @@ def partition(ilist, low, high):
             low = low+1
         i = i+1
 
+    return low
+
+
+def quick_sort(ilist, low, high):
+
+    if low < high:
+        index = partition(ilist, low, high)
+        quick_sort(ilist, low, index-1)
+        quick_sort(ilist, index+1, high)
+
+
+x = [1, 4, 8, 2, 1, 0, 2]
+quick_sort(x, 0, 6)
+print(x)
