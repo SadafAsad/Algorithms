0c91f8dc7abf8471fb767b1690ff56c13918788c
sadaf <sadaf98x@gmail.com>
2019-12-26 23:10:11 +0330

    I'M TIRED

--- Backtracking/LittleBishops.py
+++ Backtracking/LittleBishops.py
@@ -41,11 +41,26 @@ def bishopSolution(board, k, row, column):
 
     global counter
     n = len(board)
-    j = 0
+    j = column
     
-    if k == 0:
+    if k == 0 and j == n-1:
         return True
 
+    while j != n-1:
+        if isSafe(board, row, j):
+            board[row][j]=1
+            if bishopSolution(board, k-1, row, j+1):
+                counter+=1
+                board[row][j]=0
+        j+=1
+
+
+    while row != n-1:
+
+
+
+    j = 0
+    row+=1
     while j != n:
         if isSafe(board, row, j):
             board[row][j]=1
@@ -57,10 +72,28 @@ def bishopSolution(board, k, row, column):
     board[row-1][column]=0
 
 
+def bishopINLINE(board, k, row, column):
+
+    global counter
+    n = len(board)
+    j = column
+
+    if k == 0 and j == n:
+        return True
+
+    while j != n-1:
+        if isSafe(board, row, j):
+            board[row][j]=1
+            if bishopINLINE(board, k-1, row, j+1):
+                counter+=1
+                board[row][j]=0
+        j+=1
+
 counter = 0
 board = [
-    [0,0],
-    [0,0]
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
 ]
-bishopSolution(board, 1, 0, 0)
+bishopSolution(board, 2, 0, 0)
 print(counter)
