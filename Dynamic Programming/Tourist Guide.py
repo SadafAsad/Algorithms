50f3341784d0b11a6b77d9371e3b37f4e8a841bb
sadaf <sadaf98x@gmail.com>
2019-12-28 19:21:10 +0330

    example to test --with error--

--- Dynamic Programming/Tourist Guide.py
+++ Dynamic Programming/Tourist Guide.py
@@ -1,18 +1,28 @@
 
 import math
 
-def fun(matrix):
+def fun(matrix, n):
     k=0
     while k<n:
         i=0
         while i<n:
             j=0
             while j<n:
-                matrix[][]=max( matrix[i][j], min( matrix[i][k], matrix[k][j] ) )
+                matrix[i][j]=max( matrix[i][j], min( matrix[i][k], matrix[k][j] ) )
                 j+=1
             i+=1
         k+=1
 
 
 inf = math.inf
-
+matrix = [
+    [0, 30, 15, 10, inf, inf, inf],
+    [30, 0, inf, 25, 60, inf, inf],
+    [15, inf, 0, 40, inf, 20, inf],
+    [10, 25, 40, 0, inf, inf, 35],
+    [inf, 60, inf, inf, 0, inf, 20],
+    [inf, inf, 20, inf, inf, 0, 30],
+    [inf, inf, inf, 35, 20, 30, 0]
+]
+fun(matrix, 7)
+print(matrix[0][6])
