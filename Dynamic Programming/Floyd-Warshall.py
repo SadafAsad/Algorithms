59fef1ecaa71f75cf665fa89447cb5711c7a6f25
sadaf <sadaf98x@gmail.com>
2020-01-19 23:47:15 +0330

    filling dynamic table with direct paths added

--- Dynamic Programming/Floyd-Warshall.py
+++ Dynamic Programming/Floyd-Warshall.py
@@ -12,4 +12,16 @@ def initializeTable(n):
                 j+=1
             i+=1
         k+=1
-    return table 
+    return table
+
+
+def fillingTable(table, input_table):
+    n = len(input_table)
+    i=0
+    while i<n:
+        j=0
+        while j<n:
+            if input_table[i][j]!=0:
+                table[0][i][j] = input_table[i][j]
+            j+=1
+        i+=1
