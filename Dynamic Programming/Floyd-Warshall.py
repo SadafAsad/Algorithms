b67bd54808bed3c301fe3da092fd501edaef733f
sadaf <sadaf98x@gmail.com>
2020-01-20 00:14:05 +0330

    testing -- bug fixes

--- Dynamic Programming/Floyd-Warshall.py
+++ Dynamic Programming/Floyd-Warshall.py
@@ -21,21 +21,32 @@ def fillingTable(table, input_table):
     while i<n:
         j=0
         while j<n:
-            if input_table[i][j]!=0:
-                table[0][i][j] = input_table[i][j]
+            table[0][i][j] = input_table[i][j]
             j+=1
         i+=1
 
 
 def computingMins(table):
     n = len(table)
-    k=0
-    while k<n:
+    k=1
+    while k<n-1:
         i=0
         while i<n-1:
             j=0
             while j<n-1:
-                table[k][i]][j] =  min( table[k-1][i]][j] , table[k-1][i][k] + table[k-1][k][j] )
+                table[k][i][j] =  min( table[k-1][i][j] , table[k-1][i][k] + table[k-1][k][j] )
                 j+=1
             i+=1
         k+=1
+
+
+t = [[0, 5, 999, 10],
+     [999, 0, 3, 999],
+     [999, 999, 0, 1],
+     [999, 999, 999, 0]]
+
+d_t = initializeTable(len(t))
+fillingTable(d_t, t)
+computingMins(d_t)
+print(d_t)
+
